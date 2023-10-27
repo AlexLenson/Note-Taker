c63de520c70ea03e57a96ac9d884044da346d25f
Alex Lenson <144947695+AlexLenson@users.noreply.github.com>
2023-10-27 16:28:17 -0400

    get request for one note

---
+++ routes/notes.js
@@ -0,0 +1,11 @@
+const router = require("express").Router();
+const fs = require("fs");
+
+router.get("/notes", (req, res) => {
+    fs.readFile("db/db.json", "utf-8", (err, data) => {
+        if (err) throw err;
+        res.json(JSON.parse(data));
+    })
+});
+
+module.exports = router;
